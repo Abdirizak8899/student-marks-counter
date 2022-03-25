e8b2481d9db16a91cba8215d1ce282e6ac7425d0
Abdirizak8899 <102373033+Abdirizak8899@users.noreply.github.com>
2022-03-25 18:20:07 +0300

    Add files via upload

---
+++ Sudent_marks_counter.py
@@ -0,0 +1,23 @@
+# Sudent_marks counter 
+
+def Student_marks():
+    i = 0
+    limit = 3
+    
+    while i < limit:
+        print('Enter the Sudent_marks')
+        Math = int(input('Math_marks'))
+        English = int(input('English_marks'))
+        Arabic = int(input('Arabic_marks'))
+        sum = (Math + English + Arabic)
+        print('TOTAL =', sum)
+        average = (sum / 3)
+        print('average =', average)
+        if average >= 95:
+            print('High-level', 'Passed')
+        elif average > 52:
+            print('Passed')
+        else:
+            print('Failed')
+        i += 1
+Student_marks()
